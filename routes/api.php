40a17ae5e860f333fb16fea78f472da31db64a44
kareem-hub <kareemehab968@gmail.com>
2022-10-22 13:33:51 +0200

    Route fixes

--- routes/api.php
+++ routes/api.php
@@ -10,17 +10,9 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::get('/teams', [TeamsController::class, 'index'])->name('teams');
 Route::get('/teams/{team}', [TeamsController::class, 'show'])->name('teams.show');
-Route::get('/users', function (){
-    return response()->json([
-        'users' => \App\Models\User::all()
-    ]);
-})->name('users');
 
 // protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('/teams', TeamsController::class, ['except' => ['index', 'show']]);
 });
